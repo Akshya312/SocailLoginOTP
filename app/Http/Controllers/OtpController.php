c8577d2f1d5eb1393ae38e92d4c5c5dcb65136dc
akshay312 <akshaythakur2k20@gmail.com>
2024-02-26 11:07:46 +0530

    otp genrate controller

--- app/Http/Controllers/OtpController.php
+++ app/Http/Controllers/OtpController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class OtpController extends Controller
 {
-    //
+    public function sendOtp(Request $request){
+        $email  = $request->email;
+        $otp =  rand(10000,99999);
+    }
 }
