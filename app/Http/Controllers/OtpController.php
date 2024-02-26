aaeac175a5cfdd8a358e7b5f1dcc6722eb88fd2b
akshay312 <akshaythakur2k20@gmail.com>
2024-02-26 11:54:27 +0530

    send otp on email using mailer in controller

--- app/Http/Controllers/OtpController.php
+++ app/Http/Controllers/OtpController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\OtpMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Redis;
 
 class OtpController extends Controller
 {
-    public function sendOtp(Request $request){
+    public function sendOtp(Request $request)
+    {
         $email  = $request->email;
-        $otp =  rand(10000,99999);
+        $otp =  rand(10000, 99999);
+        // Store OTP in Redis with a 5-minute expiration
+        Redis::setex("otp:$email", 300, $otp);
+        // Send OTP by email
+        Mail::to($email)->send(new OtpMail($otp));
+        return response()->json([
+            'message'=> 'OTP sent successfully'
+        ]);
     }
 }
